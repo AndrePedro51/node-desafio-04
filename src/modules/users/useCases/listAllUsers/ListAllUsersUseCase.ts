6ed802b5cb51acfcce04f734d6a8887fc17847d2
AndrePedro51 <andrepedro51@gmail.com>
2022-07-01 21:30:28 -0300

    Desafio concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if(!user || user.admin === false){
+      throw new Error("Mensagem do erro");
+    }
+    
+    return this.usersRepository.list();
+    
   }
 }
 
